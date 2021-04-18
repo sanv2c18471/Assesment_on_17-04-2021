c1daa45f170025cf8299209ae6ed5911922b7fae
sanv2c18471 <80382314+sanv2c18471@users.noreply.github.com>
2021-04-18 20:49:55 +0530

    Update SuperClassmethodoveriding.java

--- sanakriassesment2/src/com/test2/SuperClassmethodoveriding.java
+++ sanakriassesment2/src/com/test2/SuperClassmethodoveriding.java
@@ -1,19 +1,22 @@
 package com.test2;
 import java.io.IOException;
 public class SuperClassmethodoveriding {
-	    void method()
-	    {
-	        System.out.println("SuperClass");
-	    }
+	public static void main(String[] args) throws Exception {
+		Dog a = new Dog();
+		a.bark("WoW_WoW");
 	}
-class SubClass extends SuperClassmethodoveriding {
-	  public void method() throws Exception
-	    {
-	        System.out.println("SubClass");
-	    }
-	    public static void main(String args[])
-	    {
-	        SuperClassmethodoveriding s = new SubClass();
-	        s.method();
-	    }
+}
+class Animal {
+
+	public void bark(String Sound) {
+		System.out.println("Animal :" + Sound);
 	}
+}
+class Dog extends Animal {
+
+	public void bark(String Sound) {
+		System.out.println("Dog:" + Sound);
+		super.bark(Sound);
+
+	}
+}
